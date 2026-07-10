752d6cd4d0ba669d655089e5fa3ca24b1dc8f46e
Alexsandro Andrade <alexsandro.mpe@gmail.com>
2026-07-10 09:23:07 -0300

    adiciona enums para Modalidade, StatusCandidatura, TipoContrato e TipoUsuario

---
+++ src/main/java/br/com/alexandrade/trampohub_api/enums/TipoUsuario.java
@@ -0,0 +1,30 @@
+package br.com.alexandrade.trampohub_api.enums;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonValue;
+
+public enum TipoUsuario {
+    EMPREGADOR("empregador"),
+    CANDIDATO("candidato");
+
+    private final String valor;
+
+    TipoUsuario(String valor) {
+        this.valor = valor;
+    }
+
+    @JsonValue
+    public String getValor() {
+        return valor;
+    }
+
+    @JsonCreator
+    public static TipoUsuario fromValor(String valor) {
+        for (TipoUsuario tipo : values()) {
+            if (tipo.valor.equalsIgnoreCase(valor)) {
+                return tipo;
+            }
+        }
+        throw new IllegalArgumentException("Tipo de usuário inválido: " + valor);
+    }
+}
